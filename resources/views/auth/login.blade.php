dc33bd114a088523d7368be51ee21b241a2809d1
yudilee <lee140685@gmail.com>
2026-03-25 15:14:29 +0700

    fix: update html title tags to dynamically use app name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Cash Bank</title>
+    <title>Login - {{ config('app.name', 'Odoo Finance SDP') }}</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Cash Bank') - Cash Bank</title>
+    <title>@yield('title', config('app.name', 'Odoo Finance SDP')) - {{ config('app.name', 'Odoo Finance SDP') }}</title>
     <link rel="icon" href="{{ asset('favicon.ico') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
